fa6ea8ec7c0bae7ab263a6bcc7453d4737d86902
hyeon9mak <jinha3507@gmail.com>
2021-07-05 23:28:59 +0900

    test: User 객체 단위 테스트 추가

--- src/test/java/qna/domain/UserTest.java
+++ src/test/java/qna/domain/UserTest.java
@@ -1,7 +1,33 @@
 package qna.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
 
     public static final User JAVAJIGI = new User(1L, "javajigi", "password", "name", "javajigi@slipp.net");
     public static final User SANJIGI = new User(2L, "sanjigi", "password", "name", "sanjigi@slipp.net");
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(3L, "jason", "password", "name", "jason@woowa.course");
+    }
+
+    @Test
+    @DisplayName("유저 정보 변경")
+    void update() {
+        // given
+        User newUser = new User("jason", "password", "newName", "newMail@woowa.course");
+
+        // when
+        user.update(user, newUser);
+
+        // then
+        assertThat(user.getEmail()).isEqualTo(newUser.getEmail());
+    }
 }
